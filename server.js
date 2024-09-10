fda438a27600d0a2f5759504153cf9497fbdb186
tassun <32536248+tassun@users.noreply.github.com>
2024-09-10 20:00:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http, {
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
 	cors: { origin: '*', }
 });
-
+let history_broadcast = [];
 const CHAT_PORT = process.env.CHAT_PORT || 9000;
 app.use(cors());
 app.use(express.static('public'));
@@ -15,19 +15,26 @@ app.get('/', function(req, res) {
 	console.log('get '+__dirname+' - send '+__dirname + '/index.html');
 	res.sendFile(__dirname + '/public/index.html');
 });
+function broadcast(message) {
+	if(message) {
+		let msg = {username: "admin", message: message};
+		io.emit("broadcast-message",msg);
+		history_broadcast.push(msg);
+		if(history_broadcast.length > 5) history_broadcast.shift();
+	}
+}
 //broadcast by admin
 app.get("/bc/:message",function(req,res) {
-	let message = req.params.message;
-	if(message) {
-		io.emit("broadcast-message",{username: "admin", message: message});
-	}
+	broadcast(req.params.message);
 	res.send("OK");
 });
 app.post("/bc",function(req,res) {
-	let message = req.params.message || req.query.message || req.body.message;
-	if(message) {
-		io.emit("broadcast-message",{username: "admin", message: message});
-	}
+	broadcast(req.params.message || req.query.message || req.body.message);
+	res.send("OK");
+});
+app.get("/bcclear",function(req,res) {
+	console.log("bc: clear ",history_broadcast.length);
+	history_broadcast = [];
 	res.send("OK");
 });
 //direct message
@@ -37,7 +44,6 @@ app.get("/direct/:id/:message",function(req,res) {
 	io.to(id).emit("chat-message",{username: "admin", message: message});
 	res.send("OK");
 });
-let history_broadcast = [];
 io.on('connection', function(socket){
 	console.log('chat:: a user connected :',socket.id);
 	for(let msg of history_broadcast) {
@@ -48,11 +54,6 @@ io.on('connection', function(socket){
 	});
 	socket.on('chat-message', function(msg){
 		io.emit('chat-message', msg);
-	});	
-	socket.on('broadcast-message', function(msg){
-		io.emit('broadcast-message', msg);
-		history_broadcast.push({...msg, id: socket.id});
-		if(history_broadcast.length > 5) history_broadcast.shift();
 	});	
 });
 http.listen(CHAT_PORT, function(){
